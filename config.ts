5b2fcf2416393572b3c06dd8a81050b88e3773a9
Carlos Benti <ti04@emilima.com.pe>
2020-06-09 11:50:29 -0500

    config.ts added

---
+++ config.ts
@@ -0,0 +1,10 @@
+const dbCreds = {
+    user: "deno",
+    database: "pgDeno",
+    password: "Deno2020*",
+    hostname: "benti.io",
+    port: 5432
+}
+
+export { dbCreds }
+/* https://deno.land/x/postgres */
